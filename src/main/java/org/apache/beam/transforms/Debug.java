c2d364f17a4f319f8969051ab50d243fb8dcd810
Ismaël Mejía <iemejia@gmail.com>
2016-03-20 00:31:50 +0100

    Add Debug transform
    It is a transform who does not change the collections, but executes a
    function given a predicate, this is useful to print information, or
    check the state of an intermediary state of a pipeline.

---
+++ src/main/java/org/apache/beam/transforms/Debug.java
@@ -0,0 +1,76 @@
+package org.apache.beam.transforms;
+
+import com.google.cloud.dataflow.sdk.annotations.Experimental;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.PTransform;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.transforms.SerializableFunction;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+
+@Experimental
+public final class Debug {
+
+  private Debug() {
+
+  }
+
+  public static <T> TransformBuilder<T>
+      when(final SerializableFunction<T, Boolean> predicate) {
+    final TransformBuilder<T> ab = new TransformBuilder<T>();
+    ab.predicate = predicate;
+    return ab;
+  }
+
+  public static <T> PTransform<PCollection<T>, PCollection<T>> print() {
+    return new TransformBuilder<T>().print();
+  }
+
+  public static <T> PTransform<PCollection<T>, PCollection<T>>
+      with(final SerializableFunction<T, Void> fun) {
+    return new TransformBuilder<T>().with(fun);
+  }
+
+  private static class NoTransformWithEffect<T>
+      extends PTransform<PCollection<T>, PCollection<T>> {
+
+    private SerializableFunction<T, Void> fun;
+    private SerializableFunction<T, Boolean> predicate = (T t) -> Boolean.TRUE;
+
+    @Override
+    public PCollection<T> apply(final PCollection<T> col) {
+      col.apply(ParDo.named("Debug.Do")
+          .of(new DoFn<T, T>() {
+            @Override
+            public void processElement(final ProcessContext c) {
+              final T elem = c.element();
+              // TODO make this more robust
+              if (predicate.apply(elem)) {
+                fun.apply(elem);
+              }
+              c.output(elem);
+            }
+          }));
+      return col;
+    }
+  }
+
+  public static class TransformBuilder<T> {
+    private SerializableFunction<T, Boolean> predicate = (T t) -> Boolean.TRUE;
+
+    public PTransform<PCollection<T>, PCollection<T>> print() {
+      return with((T t) -> {
+        System.out.println(t);
+        return null;
+      });
+    }
+
+    public PTransform<PCollection<T>, PCollection<T>>
+        with(final SerializableFunction<T, Void> fun) {
+      final NoTransformWithEffect<T> t = new NoTransformWithEffect<T>();
+      t.fun = fun;
+      t.predicate = predicate;
+      return t;
+    }
+  }
+
+}
---
+++ src/test/java/org/apache/beam/transforms/DebugTest.java
@@ -0,0 +1,56 @@
+package org.apache.beam.transforms;
+
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.coders.StringUtf8Coder;
+import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions;
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
+import com.google.cloud.dataflow.sdk.runners.DirectPipelineRunner;
+import com.google.cloud.dataflow.sdk.transforms.Create;
+import com.google.cloud.dataflow.sdk.transforms.Filter;
+import com.google.cloud.dataflow.sdk.transforms.FlatMapElements;
+import com.google.cloud.dataflow.sdk.transforms.MapElements;
+import com.google.cloud.dataflow.sdk.values.TypeDescriptor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DebugTest {
+  public static void main(final String... args) {
+    DataflowPipelineOptions options = PipelineOptionsFactory.create()
+        .as(DataflowPipelineOptions.class);
+    options.setRunner(DirectPipelineRunner.class);
+    options.setProject("SET_YOUR_PROJECT_ID_HERE");
+    Pipeline p = Pipeline.create(options);
+
+    final List<String> LINES = Arrays.asList(
+        "To be, or not to be: that is the question: ",
+        "Whether 'tis nobler in the mind to suffer ",
+        "The slings and arrows of outrageous fortune, ",
+        "Or to take arms against a sea of troubles, ");
+
+    p.apply(Create.of(LINES)).setCoder(StringUtf8Coder.of())
+        .apply(Debug.print())
+        .apply(FlatMapElements.via((String text) -> Arrays.asList(text.split(" ")))
+            .withOutputType(new TypeDescriptor<String>() {
+            }))
+        .apply(Debug.print())
+        .apply(Filter.byPredicate((String text) -> text.length() > 5))
+        .apply(Debug.print())
+        .apply(MapElements.via((String text) -> text.toUpperCase())
+            .withOutputType(new TypeDescriptor<String>() {
+            }))
+        .apply(Debug
+            .with((String s) -> {
+              System.out.println(s);
+              return null;
+            }))
+        .apply(Debug
+            .when((String s) -> s.startsWith("A"))
+            .with((String s) -> {
+              System.out.println(s);
+              return null;
+            }));
+
+    p.run();
+  }
+}
